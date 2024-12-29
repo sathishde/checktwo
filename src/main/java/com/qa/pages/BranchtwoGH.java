0580962849d9a7b3fe37cc025abb265fa9a4ab0b
Sathish Devarajulu <sathish.devarajulu@gmail.com>
2024-12-29 23:16:14 +0300

    new changes

--- src/main/java/com/qa/pages/BranchtwoGH.java
+++ src/main/java/com/qa/pages/BranchtwoGH.java
@@ -2,7 +2,7 @@ package com.qa.pages;
 
 public class BranchtwoGH {
 
-    public void homePage(){
+    public void branchTwoPage(){
         System.out.println("branch two test page Method from GH");
     }
 
